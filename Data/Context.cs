82223abec6acbbfed0967ebbb1c6cc4e6cc8fde9
Vlad Zaharia <vlad@zaharia.io>
2020-07-27 14:28:49 -0700

    Initial commit

---
+++ Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GGStream.Models;
+
+namespace GGStream.Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GGStream.Models.Stream> Stream { get; set; }
+
+        public DbSet<GGStream.Models.Collection> Collection { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GGStream.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
