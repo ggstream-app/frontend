332c21fd36ac472aa6b5144a870190f5e0362c9f
Vlad Zaharia <vlad@zaharia.io>
2020-07-27 14:31:35 -0700

    Add basic VCs

---
+++ Controllers/CollectionsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GGStream.Data;
+using GGStream.Models;
+
+namespace GGStream.Controllers
+{
+    public class CollectionsController : Controller
+    {
+        private readonly Context _context;
+
+        public CollectionsController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Collections
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Collection.ToListAsync());
+        }
+
+        // GET: Collections/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var collection = await _context.Collection
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (collection == null)
+            {
+                return NotFound();
+            }
+
+            return View(collection);
+        }
+
+        // GET: Collections/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Collections/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,URL,BaseColor,Private,ShowHowTo,TeamsLink")] Collection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                collection.Id = Guid.NewGuid();
+                _context.Add(collection);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(collection);
+        }
+
+        // GET: Collections/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var collection = await _context.Collection.FindAsync(id);
+            if (collection == null)
+            {
+                return NotFound();
+            }
+            return View(collection);
+        }
+
+        // POST: Collections/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,URL,BaseColor,Private,ShowHowTo,TeamsLink")] Collection collection)
+        {
+            if (id != collection.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(collection);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CollectionExists(collection.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(collection);
+        }
+
+        // GET: Collections/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var collection = await _context.Collection
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (collection == null)
+            {
+                return NotFound();
+            }
+
+            return View(collection);
+        }
+
+        // POST: Collections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var collection = await _context.Collection.FindAsync(id);
+            _context.Collection.Remove(collection);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CollectionExists(Guid id)
+        {
+            return _context.Collection.Any(e => e.Id == id);
+        }
+    }
+}
