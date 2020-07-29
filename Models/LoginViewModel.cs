833ca535198ab07b8ae227587977b30a680ab037
Vlad Zaharia <vlad@zaharia.io>
2020-07-29 03:53:46 -0700

    Add AAD Auth + Editing links (#6)
    * Tons of changes
    
    * Edit throughout

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GGStream.Models
+{
+    public class LoginViewModel
+    {
+        public Boolean HideLoginUI { get; set; }
+    }
+}
